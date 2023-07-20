4bfd8972815ac85a95be49aff21a789fedc29f84
Kyant0 <kyant2021@outlook.com>
2023-07-20 16:44:30 +0800

    Add taglib submodule

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.kyant.tagapp"
+    compileSdk = 34
+    buildToolsVersion = "34.0.0"
+
+    defaultConfig {
+        applicationId = "com.kyant.tagapp"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+        languageVersion = "2.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+    packaging {
+        resources {
+            excludes += arrayOf("/META-INF/{AL2.0,LGPL2.1}", "DebugProbesKt.bin")
+        }
+        dex {
+            useLegacyPackaging = true
+        }
+        jniLibs {
+            useLegacyPackaging = true
+        }
+    }
+    lint {
+        checkReleaseBuilds = false
+    }
+}
+
+dependencies {
+    implementation(project(":lib"))
+
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.compose.ui)
+    implementation(libs.compose.material3)
+
+    implementation(libs.androidx.activity.compose)
+}
